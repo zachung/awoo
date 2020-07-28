70aa0a25da759c68bfb6587e3eb6003925a700e0
zachung <zach50931@gmail.com>
2020-07-28 16:07:25 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # awoo
+install dependencies
+```shell script
+npm i
+```
+build
+```shell script
+npm run build
+```
+install http-server for ajax request world maps (prevent Cross origin requests)
+```shell script
+npm install -g http-server
+```
+and run up
+```shell script
+http-server
+```
---
+++ src/lib/Game.js
@@ -0,0 +1,54 @@
+import { Stage, Item, Symbols } from 'awoo-core'
+
+/**
+ * @property {Stage} stage
+ */
+class Game {
+  constructor (props) {
+    const { viewSize, cameraDelta } = props
+    this.stage = new Stage({
+      viewSize,
+      cameraDelta
+    })
+  }
+
+  start () {
+    const initX = 16
+    const initY = 16
+    return Promise.resolve()
+      .then(() => {
+        return this.addPlayer({ x: initX, y: initY })
+      })
+      .then(player => {
+        this.startRender()
+        return player
+      })
+  }
+
+  addPlayer ({ x, y }) {
+    const player = new Item(Symbols.Items[0])
+    player.location = { x, y }
+    return this.stage.cameraGoTo(x, y)
+      .then(() => {
+        this.stage.chunk.addItem(player)
+        this.player = player
+        return player
+      })
+  }
+
+  startRender () {
+    // timer for render
+    setInterval(() => {
+      this.stage.cameraGoTo(this.player.location.x, this.player.location.y)
+        .catch(status => {
+          if (status === 404) {
+            console.log('Map limited')
+            return
+          }
+          console.log(status)
+        })
+    }, 100)
+  }
+}
+
+export default Game
