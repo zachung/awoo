2c8bc0729180535853505f637e3d2c756c32fab7
zachung <zach50931@gmail.com>
2020-08-05 18:06:38 +0800

    feat.better connect form

--- src/ui/ConnectComponent.vue
+++ src/ui/ConnectComponent.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
-    <label for="uri">server uri</label>
-    <input id="uri" v-model="uri" type="text" />
-    <label for="name">name</label>
-    <input id="name" v-model="name" type="text" />
-    <button @click="$emit('connect', uri, name)">Connect</button>
+    <form action="#" @submit.prevent="connect">
+      <label for="uri">server uri</label>
+      <input id="uri" v-model="uri" type="text" />
+      <label for="name">name</label>
+      <input id="name" v-model="name" type="text" />
+      <button type="submit">Connect</button>
+
+      <div v-if="errors.length" style="color: red">
+        <ul>
+          <li v-for="error in errors">{{ error }}</li>
+        </ul>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -12,9 +20,24 @@
 export default {
   data() {
     return {
+      errors: [],
       uri: "http://localhost:3000",
       name: ""
     };
+  },
+  methods: {
+    connect() {
+      this.errors = [];
+      if (!this.name) {
+        this.errors.push("Name required.");
+        return;
+      }
+      if (!this.uri) {
+        this.errors.push("Server uri required.");
+        return;
+      }
+      this.$emit("connect", this.uri, this.name);
+    }
   }
 };
 </script>
